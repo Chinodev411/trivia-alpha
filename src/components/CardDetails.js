f57cfae61fbffde6f73266ec581ba4559ea058f6
Chinodev411 <Chinodev@outlook.com>
2021-07-02 08:31:02 -0400

    update

--- src/components/CardDetails.js
+++ src/components/CardDetails.js
@@ -1,9 +1,7 @@
 import React from 'react'
 
-// import { useState, useEffect } from 'react';
 
 const CardDetails = (props) => {
-  // console.log(props);
   return (
     <div className='details-container'>
       {props.match.params.correct_answer}
